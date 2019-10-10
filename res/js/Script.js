58d6f3af67e165d6219452faeb291e59c15a5322
sanderh <sander.harjak@nortal.com>
2019-10-10 23:22:02 +0300

    Created courses array and initialized courses table body dynamically with jQuery

--- res/js/Script.js
+++ res/js/Script.js
@@ -1,5 +1,11 @@
 $(function () {
-    var user = new User("Jaanus", "Talvik", "21/04/1997", "Software Development", 4.35);
+    let user = new User("Jaanus", "Talvik", "21/04/1997", "Software Development", 4.35);
+    let courses = [
+        new Course("Object-oriented programming", 1, 95),
+        new Course("Programming in Python", 1, 100),
+        new Course("Data Structures and Algorithms", 2, 85),
+        new Course("Introduction to artificial intelligence", 2, 88)
+    ];
     init();
 
     function init() {
@@ -7,5 +13,20 @@ $(function () {
         $("#birthdate").text(user.birthdate);
         $("#faculty").text(user.faculty);
         $("#gpa strong").text(user.gpa);
+
+        let coursesTableBody = $("#courses tbody");
+        for (let i = 0; i < courses.length; i++) {
+            let tr = $("<tr></tr>");
+            let rowId = $("<td></td>").text(i + 1);
+            let courseTitle = $("<td></td>").text(courses[i].title);
+            let semester = $("<td></td>").text(courses[i].semester);
+            let grade = $("<td></td>").text(courses[i].grade);
+            tr.append(rowId);
+            tr.append(courseTitle);
+            tr.append(semester);
+            tr.append(grade);
+            coursesTableBody.append(tr);
+
+        }
     }
 });
